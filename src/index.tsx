1b0b03919b8795d4e0c38475fa39cfbcd5ee3ad6
Karthikeyan Ranasthala <karthikeyan.ranasthala@gmail.com>
2022-08-14 19:10:10 +0530

    Add Snap variant

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,35 @@ import { Component, createSignal, JSX, onCleanup, onMount } from "solid-js";
 
 const DEFAULT_THRESHOLD = 50;
 
-export interface SolidBottomsheetProps {
+export interface BaseSolidBottomsheetProps {
   children: JSX.Element;
   onClose: () => void;
 }
 
+export interface DefaultVariantProps extends BaseSolidBottomsheetProps {
+  variant: "default";
+}
+
+export interface SnapVariantProps extends BaseSolidBottomsheetProps {
+  variant: "snap";
+  defaultSnapPoint: ({ maxHeight }: { maxHeight: number }) => number;
+  snapPoints: ({ maxHeight }: { maxHeight: number }) => number[];
+}
+
+export type SolidBottomsheetProps = DefaultVariantProps | SnapVariantProps;
+
 export const SolidBottomsheet: Component<SolidBottomsheetProps> = (props) => {
+  const isSnapVariant = props.variant === "snap";
+
+  const snapPoints = isSnapVariant
+    ? [
+        0,
+        ...props
+          .snapPoints({ maxHeight: window.innerHeight })
+          .sort((a, b) => b - a),
+      ]
+    : [];
+
   let touchStartPosition = 0;
   let lastTouchPosition = 0;
 
@@ -21,31 +44,80 @@ export const SolidBottomsheet: Component<SolidBottomsheetProps> = (props) => {
   });
 
   const [isClosing, setIsClosing] = createSignal(false);
-  const [bottomsheetTranslatePosition, setBottomsheetTranslatePosition] =
-    createSignal(0);
+  const [isSnapping, setIsSnapping] = createSignal(false);
+  const [bottomsheetTranslateValue, setBottomsheetTranslateValue] =
+    createSignal(
+      isSnapVariant
+        ? props.defaultSnapPoint({ maxHeight: window.innerHeight })
+        : 0
+    );
 
   const onTouchStart: JSX.EventHandlerUnion<HTMLDivElement, TouchEvent> = (
     event
   ) => {
-    touchStartPosition = event.touches[0].screenY;
+    isSnapVariant && setIsSnapping(false);
+
+    touchStartPosition = lastTouchPosition = event.touches[0].screenY;
   };
 
   const onTouchMove: JSX.EventHandlerUnion<HTMLDivElement, TouchEvent> = (
     event
   ) => {
-    lastTouchPosition = event.touches[0].screenY;
+    let dragDistance = 0;
+    switch (props.variant) {
+      case "snap":
+        dragDistance = event.touches[0].clientY - lastTouchPosition;
 
-    const dragDistance = lastTouchPosition - touchStartPosition;
-    if (dragDistance > 0) {
-      setBottomsheetTranslatePosition(dragDistance);
+        setBottomsheetTranslateValue((previousVal) =>
+          Math.min(Math.max(previousVal + dragDistance, 0), window.innerHeight)
+        );
+
+        lastTouchPosition = event.touches[0].clientY;
+
+        break;
+      case "default":
+      default:
+        lastTouchPosition = event.touches[0].screenY;
+        dragDistance = lastTouchPosition - touchStartPosition;
+
+        if (dragDistance > 0) {
+          setBottomsheetTranslateValue(dragDistance);
+        }
+
+        break;
     }
   };
 
   const onTouchEnd: JSX.EventHandlerUnion<HTMLDivElement, TouchEvent> = () => {
-    if (lastTouchPosition - touchStartPosition > DEFAULT_THRESHOLD) {
-      setIsClosing(true);
-    } else {
-      setBottomsheetTranslatePosition(0);
+    switch (props.variant) {
+      case "snap":
+        const currentPoint = window.innerHeight - lastTouchPosition;
+
+        const closest = snapPoints.reduce((previousVal, currentVal) => {
+          return Math.abs(currentVal - currentPoint) <
+            Math.abs(previousVal - currentPoint)
+            ? currentVal
+            : previousVal;
+        });
+
+        if (closest === 0) {
+          setIsClosing(true);
+          break;
+        }
+
+        setIsSnapping(true);
+        setBottomsheetTranslateValue(window.innerHeight - closest);
+
+        break;
+      case "default":
+      default:
+        if (lastTouchPosition - touchStartPosition > DEFAULT_THRESHOLD) {
+          setIsClosing(true);
+        } else {
+          setBottomsheetTranslateValue(0);
+        }
+
+        break;
     }
   };
 
@@ -64,9 +136,11 @@ export const SolidBottomsheet: Component<SolidBottomsheetProps> = (props) => {
           classList={{
             "sb-content": true,
             "sb-is-closing": isClosing(),
+            "sb-snap-variant": isSnapVariant,
+            "sb-is-snapping": isSnapping(),
           }}
           style={{
-            transform: `translateY(${bottomsheetTranslatePosition()}px)`,
+            transform: `translateY(${bottomsheetTranslateValue()}px)`,
           }}
           {...(isClosing() ? { onAnimationEnd: props.onClose } : {})}
         >
